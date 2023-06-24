fe58ec28cf13f49d51b558f444a12dad94be0343
chandiosamad07 <absamadchandio@gmail.com>
2023-06-24 17:21:37 +0500

    backend done

--- frontend/src/components/Creation.js
+++ frontend/src/components/Creation.js
@@ -3,26 +3,26 @@ import React from 'react'
 function Creation() {
   return (
     <div>
-      <label>Recipe title</label>
-      <input type='text'></input>
+      <label >Recipe title</label>
+      <input type='text' id='title'></input>
       <br></br>
       <br></br>
         <br></br>
       <label>Description</label>
-      <input type='text'></input>
+      <input type='text' id='description'></input>
       <br></br>
       <br></br>
         <br></br>
       <label>Ingredients</label>
-      <input type='text'></input>
+      <input type='text' id='Ing'></input>
       <br></br>
       <br></br>
         <br></br>
       <label>Instrucation</label>
-      <input type='text'></input>
+      <input type='text' id='ins'></input>
         <br></br>
         <br></br>
-        <button type='sumit' onClick={alert("submit")}> Submit </button>
+        <button type='sumit'> Submit </button>
     </div>
   )
 }
