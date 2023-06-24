07471c5bbe456ba969474ee5c26caa38d1eac2be
chandiosamad07 <absamadchandio@gmail.com>
2023-06-24 15:49:19 +0500

    controller file

--- backend/models/Recipe.js
+++ backend/models/Recipe.js
@@ -3,12 +3,13 @@ import mongoose from "mongoose";
 // Defining Schema
 
 const RecipeSchema = new mongoose.Schema({
-    name: { type: String, required: true, trim: true },
-    age: { type: Number, required: true, min: 18, max: 60 },
-    fees: {type: mongoose.Decimal128, required: true, validate: (v) => v >= 5000}
+    tilte: { type: String, required: true, trim: true },
+    Des:  { type: String, required: true, trim: true },
+    ins:  { type: String, required: true, trim: true },
+    Ing: { type: String, required: true, trim: true },
 })
 
-// Model
+
 const RecipeModel = mongoose.model("Recipe", RecipeSchema)
 
 export default RecipeModel
