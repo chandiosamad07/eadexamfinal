0b6bd3716f171a238cb29f04392d93eedcc31247
chandiosamad07 <absamadchandio@gmail.com>
2023-06-24 15:12:10 +0500

    Creation.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Creation from './components/Creation';
+
+function App() {
+  return (
+    <div>
+    <Creation></Creation>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Creation.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Creation() {
+  return (
+    <div>
+      <label>Recipe title</label>
+      <input type='text'></input>
+      <br></br>
+      <label>Description</label>
+      <input type='text'></input>
+      <br></br>
+      <label>Ingredients</label>
+      <input type='text'></input>
+      <br></br>
+      <label>Instrucation</label>
+      <input type='text'></input>
+
+    </div>
+  )
+}
+
+export default Creation
